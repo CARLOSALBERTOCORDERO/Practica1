5e03a834e8519e71914dd50ba0e3c23b9744f19b
CARLOS_CORDERO <ms197686@iteso.mx>
2019-09-11 23:26:24 -0500

    [CCORDERO] encintcomm_private_added

---
+++ macbase/boards/frdmk64f_frdmcr20a/wireless_examples/ieee_802_15_4/msn_coordinator/freertos/kds/encintcomm/encintcomm_private.h
@@ -0,0 +1,15 @@
+/*
+ * encintcomm_private.h
+ *
+ *  Created on: 11/09/2019
+ *      Author: CARLOS_CORDERO
+ */
+
+#ifndef ENCINTCOMM_ENCINTCOMM_PRIVATE_H_
+#define ENCINTCOMM_ENCINTCOMM_PRIVATE_H_
+
+
+extern void encintcomm_task(void* argument);
+
+
+#endif /* ENCINTCOMM_ENCINTCOMM_PRIVATE_H_ */
